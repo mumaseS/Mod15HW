e30a84772ce48a9098a5935db28f7a522b0275d5
Sesamum Somniferum <greedyogre@outlook.com>
2022-10-03 20:16:01 +0300

    Добавьте файлы проекта.

---
+++ Mod15HW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+        };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var result = classes.SelectMany(s => s.Students).ToArray();
+        return result;
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+
+}
